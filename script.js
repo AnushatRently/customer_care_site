e94b23b8bc9a0bbfee938e6d9c0f5c67a024c345
Anush <anushkumar.s@rently.com>
2022-11-21 21:32:22 +0530

    demo_2

--- script.js
+++ script.js
@@ -1,34 +1,137 @@
-const socket = io('http://localhost:3000')
-const messageContainer = document.getElementById('message-container')
-const messageForm = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
+const socket = io('http://localhost:5000')
+// const messageContainer = document.getElementById('message-container')
+// const messageForm = document.getElementById('send-container')
+// const messageInput = document.getElementById('message-input')
+const send_button=document.getElementById('send_button')
 
-const name = prompt('What is your name?')
-appendMessage('You joined')
-socket.emit('new-user', name)
+send_button.style.visibility = 'hidden';
 
-socket.on('chat-message', data => {
-  appendMessage(`${data.name}: ${data.message}`)
+//const name = prompt('What is your name?')
+// appendMessage('You joined')
+// socket.emit('new-user', name)
+
+// socket.on('connect', () => {
+//   console.log(`connect:${socket.id}`);
+// });
+
+// socket.on('chat-message', data => {
+//   appendMessage(`${data.name}: ${data.message}`)
+// })
+
+socket.on('display_event-1', data => {
+  appendMessage(data.message, data.name)
 })
 
-socket.on('user-connected', name => {
-  appendMessage(`${name} connected`)
+socket.on('handover_started', () => {
+  var accepted = confirmation()
+  allowOperator(accepted)
 })
 
-socket.on('user-disconnected', name => {
-  appendMessage(`${name} disconnected`)
-})
+function confirmation() {
+  return confirm('Operator Requested. Can you join in chat please?');
+}
 
-messageForm.addEventListener('submit', e => {
+function allowOperator(accepted) {
+  if(accepted) {
+    send_button.style.visibility = 'visible'
+  }else {
+    message = 'Currently agent is not available'
+    appendMessage(message, 'Bot')
+    socket.emit('event-2', message)
+  };
+}
+
+// socket.on('user-connected', name => {
+//   appendMessage(`${name} connected`)
+// })
+
+// socket.on('user-disconnected', name => {
+//   appendMessage(`${name} disconnected`)
+// })
+const sendButton = document.getElementById('send_button')
+sendButton.addEventListener('click', e => {
   e.preventDefault()
-  const message = messageInput.value
-  appendMessage(`You: ${message}`)
-  socket.emit('send-chat-message', message)
-  messageInput.value = ''
+  board = document.getElementById('content_display_id')
+  message = document.getElementById('content_box_id')
+  content = botMessage(message.value)
+  board.append(content)
+  socket.emit('event-2', message.value)
+  message.value = ''
 })
+// messageForm.addEventListener('submit', e => {
+//   e.preventDefault()
+//   // const message = messageInput.value
+//   // appendMessage(`Agent: ${message}`)
+//   // socket.emit('event-2', message)
+//   console.log("Cool")
+//   // messageInput.value = ''
+// })
 
-function appendMessage(message) {
-  const messageElement = document.createElement('div')
-  messageElement.innerText = message
-  messageContainer.append(messageElement)
+// function appendMessage(message) {
+//   const messageElement = document.createElement('div')
+//   messageElement.innerText = message
+//   messageContainer.append(messageElement)
+// }
+
+function botMessage(message) {
+  new_row = document.createElement('div');
+  new_row.className = "chat__conversation-board__message-container";
+  new_row2 = document.createElement('div');
+  new_row2.className = "chat__conversation-board__message__person";
+  new_row3 = document.createElement('div');
+  new_row3.className = "chat__conversation-board__message__person__avatar";
+  img = document.createElement("img");
+  img.src = "https://randomuser.me/api/portraits/women/44.jpg";
+  new_row3.appendChild(img)
+  new_row4 = document.createElement('div');
+  new_row4.className = "chat__conversation-board__message__context";
+  content =  document.createElement('div');
+  content.className = "chat__conversation-board__message__bubble";
+  span_tag = document.createElement("SPAN");
+  content1 = document.createTextNode(message);
+  span_tag.appendChild(content1)
+  content.appendChild(span_tag)
+  new_row4.appendChild(content);
+  new_row2.appendChild(new_row3)
+  new_row.appendChild(new_row2)
+  new_row.appendChild(new_row4)
+  return new_row
+}
+
+function userMessage(message) {
+  new_row = document.createElement('div');
+  new_row.className = "chat__conversation-board__message-container reversed";
+  new_row2 = document.createElement('div');
+  new_row2.className = "chat__conversation-board__message__person";
+  new_row3 = document.createElement('div');
+  new_row3.className = "chat__conversation-board__message__person__avatar";
+  img = document.createElement("img");
+  img.src = "https://randomuser.me/api/portraits/men/9.jpg";
+  new_row3.appendChild(img)
+  new_row4 = document.createElement('div');
+  new_row4.className = "chat__conversation-board__message__context";
+  content =  document.createElement('div');
+  content.className = "chat__conversation-board__message__bubble";
+  span_tag = document.createElement("SPAN");
+  content1 = document.createTextNode(message);
+  span_tag.appendChild(content1)
+  content.appendChild(span_tag)
+  new_row4.appendChild(content);
+  new_row2.appendChild(new_row3)
+  new_row.appendChild(new_row2)
+  new_row.appendChild(new_row4)
+  return new_row
+}
+
+function appendMessage(message, sender) {
+  board = document.getElementById('content_display_id')
+  if (sender === 'Bot') {
+    board = document.getElementById('content_display_id')
+    new_content = botMessage(message)
+    board.append(new_content)
+  } else {
+    board = document.getElementById('content_display_id')
+    new_content = userMessage(message)
+    board.append(new_content)
+  }
 }
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const io = require('socket.io')(3000)
+const io = require("socket.io")(5000, {
+  cors: {
+    origin: "*"
+  }
+});
 
 const users = {}
 
+// io.listen(5000, ()=>{
+//   console.log("Application running successfully on port: ");
+// });
+
 io.on('connection', socket => {
+
   socket.on('new-user', name => {
     users[socket.id] = name
     socket.broadcast.emit('user-connected', name)
@@ -13,5 +22,20 @@ io.on('connection', socket => {
   socket.on('disconnect', () => {
     socket.broadcast.emit('user-disconnected', users[socket.id])
     delete users[socket.id]
+  })
+  socket.on("connect_error", (err) => {
+    console.log(`connect_error due to ${err.message}`);
+  });
+  socket.on("event-1", message => {
+    socket.broadcast.emit('display_event-1', {message: message.message, name: message.user})
+    console.log('Broadcasted')
+  });
+
+  socket.on("event-2", message => {
+    socket.broadcast.emit('display_event-2', message)
+    console.log('Broadcasted')
+  });
+  socket.on('triggered_handoff', () => {
+    socket.broadcast.emit('handover_started')
   })
 })
